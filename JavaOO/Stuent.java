7d6c0f1af3fee7a88010bdb37e496199b1567b4b
spk1945 <hhp1945@naver.com>
2017-04-04 13:36:47 +0900

    1번
    String형의 학과와 정수형의 학번을 필드로선언.
    Student 클래스의 main()메소드에서 Student 객체를 생성하여 학과와 학번 필드에 적당한 값을 입력 후 출력.

---
+++ JavaOO/Stuent.java
@@ -0,0 +1,17 @@
+
+public class Stuent {
+	
+	String hackgwa;
+	int number;
+
+	public static void main(String[] args) {
+			Stuent s1 = new Stuent();
+			s1.hackgwa = "컴퓨터정보공학";
+			s1.number = 20140000;
+
+			System.out.println("학과 : "+s1.hackgwa);
+			System.out.println("학번 : "+s1.number);
+			
+	}
+
+}
